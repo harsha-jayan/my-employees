bfa959ad8fc093efae5d4e81b251a6e3b335e1ed
harsha-jayan <harshajayan18@gmail.com>
2021-04-04 14:34:09 +0530

    initial commit

---
+++ src/test/java/com/sample/myemployees/MyEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.myemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
